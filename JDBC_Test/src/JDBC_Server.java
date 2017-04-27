a781a9831107fb711633024bf250d53276785fb7
mackncheesiest <mackncheesiest@gmail.com>
2017-04-27 12:07:08 -0700

    Removed hardcoded login & switched to PreparedStatement SQL Queries

--- JDBC_Test/src/JDBC_Server.java
+++ JDBC_Test/src/JDBC_Server.java
@@ -12,6 +12,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.System.exit;
 
@@ -24,15 +25,14 @@ public class JDBC_Server {
 
     public static void main(String[] args) throws IOException {
 
-        //TODO: Allow the user to pass in these parameters through args
         //Initialize the database connection
         //The basic idea is along the lines of
         //                    use jdbc thin driver to connect to user hr @ localhost through port 1521, and it's an eXpress Edition of Oracle Database
         final String oracleURL = "jdbc:oracle:thin:hr/hr@localhost:1521/XE";
-        //The user's name is josh
-        final String username = "josh";
-        //And the password is incredibly secure
-        final String password = "OracleDB";
+        //The account used for development is "josh";"OracleDB"
+        //Although, of course, it just depends on your database setup
+        String username;
+        String password;
 
         Connection dbconn = null;
 
@@ -45,16 +45,24 @@ public class JDBC_Server {
             exit(-1);
         }
 
-        //Try to actually connect to the Oracle Database server
-        try {
-            dbconn = DriverManager.getConnection(oracleURL, username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            exit(-1);
+        while (dbconn == null) {
+            Scanner myScanner = new Scanner(System.in);
+
+            System.out.print("Username: ");
+            username = myScanner.next();
+            System.out.print("Password: ");
+            password = myScanner.next();
+
+            //Try to actually connect to the Oracle Database server
+            try {
+                dbconn = DriverManager.getConnection(oracleURL, username, password);
+            } catch (SQLException e) {
+                System.out.println("Login to database failed...\n");
+                System.out.flush();
+            }
         }
 
         //So if we've made it this far, we've connected to the database. Now, just startup the remaining server pieces
-
         //Open the ServerSocket
         sock = new ServerSocket(SERVER_PORT);
 
@@ -104,99 +112,90 @@ class ClientHandler extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-
-            String sql = null;
-
-            // Read a String from the client that will be used to construct our SQL command
-            try {
-                sql = (String) input.readObject();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                this.cleanup();
-                return;
-            }
-            //If the user sent nothing or something didn't read properly (but wasn't caught above), just quit this thread
-            if (sql == null) {
-                this.cleanup();
-                return;
-            }
-
-            //Now that we have a valid string, let's build an SQL statement out of it, execute it, and return the results to the client
-            try {
-                Statement stmt = dbconn.createStatement();
-
-                System.out.println("Recieved string " + sql);
-                //Execute the actual SQL query -- a select query that assumes a table BCTABLE exists and it has elements
-                //With fields (id, name) each VARCHAR2(256) and id as the PRIMARY KEY
-                //ResultSet rs = stmt.executeQuery("SELECT LINE, MODULE, SLOT FROM MOCKCONTINENTAL1 WHERE BARCODE = " + sql + "\n");
-                System.out.println("SQL Statement: " + "SELECT ID, LENUM_IST FROM IMPORTED_CONTINENTAL_SAP WHERE MATNR = " + sql + "\n");
-                ResultSet rs = stmt.executeQuery("SELECT ID, LENUM_IST FROM IMPORTED_CONTINENTAL_SAP WHERE MATNR = '" + sql + "'\n");
-
-                int minOrderNumber = Integer.MAX_VALUE;
-                String ReelID = "";
-                int tempOrderNum;
-                while (rs.next()) {
-                    System.out.println("There was a match in the SAP database");
-                    System.out.println("ID: " + rs.getString("ID") + ", REEL_ID: " + rs.getString("LENUM_IST"));
-                    tempOrderNum = rs.getInt("ID");
-                    if (tempOrderNum < minOrderNumber) {
-                        minOrderNumber = tempOrderNum;
-                        ReelID = rs.getString("LENUM_IST");
-                    }
-                }
-
-                System.out.println("I chose ID: " + minOrderNumber + ", REEL_ID: " + ReelID);
-
-                //Now select the line, module, and slot pieces from the second database where the ReelID matches
-                rs = stmt.executeQuery("SELECT Buffer, Slot from IMPORTED_CONTINENTAL_PICKS2 WHERE SUD = '" + ReelID + "'\n");
-
-                //There should only be one match for this, but prepare a boilerplate response for if the result set is empty
-                ArrayList<String> headsetResults = new ArrayList<String>();
-
-                String Line = "No data found for this barcode in PICKS2 database";
-                String Module = "";
-                String Slot = "";
-
-                while (rs.next()) {
-                    String buffer = rs.getString("Buffer");
-                    //i.e. NG24NX1M09 has Line = 24, Module = 09
-                    Line = buffer.substring(2, 4);
-                    Module = buffer.substring(buffer.length() - 2);
-
-                    String moduleSlot = rs.getString("Slot");
-                    Slot = moduleSlot.substring(moduleSlot.length()-2);
-                }
-
-                headsetResults.add(Line);
-                headsetResults.add(Module);
-                headsetResults.add(Slot);
-
-                /*
-                //Note that a ResultSet does not appear to actually contain the data. Rather, it contains the information needed to find it in the database
-                //Thus, we need to first extract the data from the database before sending it back to the client
-                ArrayList<String> temp = new ArrayList<String>();
-
-                //In our use case, I'm pretty sure there will basically only be one match for each SELECT query, but just in case, send them all back
-                //Iterate through all the matching rows (i.e. only one)
-                while (rs.next()) {
-                    //Extract the pieces we need from it
-                    temp.add(rs.getString("LINE"));
-                    temp.add(rs.getString("MODULE"));
-                    temp.add(rs.getString("SLOT"));
-                }
-                */
-
-                //Physically send the results back to the client
-                output.writeObject(headsetResults);
-                System.out.println("Replied back with an ArrayList of " + headsetResults.size() + " elements");
-
-            } catch (SQLException | IOException e) {
-                e.printStackTrace();
-            }
+        String barcode = null;
+        // Read a String from the client that will be used to construct our SQL command
+        try {
+            barcode = (String) input.readObject();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            this.cleanup();
+            return;
+        }
+        //If the user sent nothing or something didn't read properly (but wasn't caught above), just quit this thread
+        if (barcode == null) {
+            this.cleanup();
+            return;
         }
 
+        //Now that we have a valid string, let's build an SQL statement out of it, execute it, and return the results to the client
+        try {
+            //TODO: We use PreparedStatements, but OWASP also recommends potentially sanitizing the input still
+            //Use PreparedStatements to help prevent SQL Injection
+            //https://www.owasp.org/index.php/SQL_Injection_Prevention_Cheat_Sheet
+            //This is the query we want to execute
+            String query = "SELECT ID, LENUM_IST FROM IMPORTED_CONTINENTAL_SAP WHERE MATNR = ? ";
+            //So prepare a statement for it so that we know anything else we add is to be interpreted as data, not SQL commands
+            PreparedStatement pstmt = dbconn.prepareStatement(query);
+            //Set the prepared statement parameter
+            pstmt.setString(1, barcode);
+            //And execute the SQL query
+            ResultSet rs = pstmt.executeQuery();
+
+            //Find the entry in the SAP database that matches our barcode and has the smallest ID
+            int minOrderNumber = Integer.MAX_VALUE;
+            String ReelID = "";
+            int tempOrderNum;
+            while (rs.next()) {
+                //System.out.println("There was a match in the SAP database");
+                //System.out.println("ID: " + rs.getString("ID") + ", REEL_ID: " + rs.getString("LENUM_IST"));
+                tempOrderNum = rs.getInt("ID");
+                if (tempOrderNum < minOrderNumber) {
+                    minOrderNumber = tempOrderNum;
+                    ReelID = rs.getString("LENUM_IST");
+                }
+            }
+
+            System.out.println("I chose ID: " + minOrderNumber + ", REEL_ID: " + ReelID);
+
+            //Repeat the process with the second database using what we learned from the first one
+            //Now select the line, module, and slot pieces from the second database where the ReelID matches
+            query = "SELECT Buffer, Slot from IMPORTED_CONTINENTAL_PICKS2 WHERE SUD = ? ";
+            pstmt = dbconn.prepareStatement(query);
+            pstmt.setString(1, ReelID);
+            rs = pstmt.executeQuery();
+
+            //There should only be one match for this, but prepare a boilerplate response for if the result set is empty
+            ArrayList<String> headsetResults = new ArrayList<String>();
+
+            String Line = "No data found for this barcode in PICKS2 database";
+            String Module = "";
+            String Slot = "";
+
+            while (rs.next()) {
+                String buffer = rs.getString("Buffer");
+                //i.e. NG24NX1M09 has Line = 24, Module = 09
+                Line = buffer.substring(2, 4);
+                Module = buffer.substring(buffer.length() - 2);
+
+                String moduleSlot = rs.getString("Slot");
+                Slot = moduleSlot.substring(moduleSlot.length()-2);
+            }
+
+            headsetResults.add(Line);
+            headsetResults.add(Module);
+            headsetResults.add(Slot);
+
+            //Physically send the results back to the client
+            output.writeObject(headsetResults);
+            System.out.println("Replied back with an ArrayList of " + headsetResults.size() + " elements");
+            //Tear down the connection
+            //Even though, without this, it manages to tear itself down anyway by trying to read a second barcode and failing
+            cleanup();
+
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Closes all the resources of a ClientHandler and logs a message.
@@ -206,7 +205,7 @@ class ClientHandler extends Thread {
         try {
             this.input.close();
             this.output.close();
-            //DONT close dbconn as it is a shared connection among all the clients
+            //DON'T close dbconn as it is the server's connection to the database itself.
             System.out.println("Closed connection!");
         } catch (IOException e) {
             e.printStackTrace();
